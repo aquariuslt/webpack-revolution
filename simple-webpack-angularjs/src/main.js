c1386cb6bbf94132c8fdc5aa85ca9ba5e774a313
Jason Cui <jason.cui@oocl.com>
2017-05-19 11:03:25 +0800

    [Example] add simple-webpack-angularjs example.

---
+++ simple-webpack-angularjs/src/main.js
@@ -0,0 +1,14 @@
+/** Created by CUIJA on 05-19-2017.*/
+
+var angular = require('angular');
+var uirouter = require('@uirouter/angularjs');
+
+
+// import your routes config
+var appRoutes = require('./app/routes');
+
+module.exports = angular.module('ita-app', [
+  'ui.router'
+])
+  .config(appRoutes)
+;
